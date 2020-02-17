3167962d332c57d8d409e688c9e8ea595b0df618
wayne <e28s32is@gmail.com>
2020-02-17 23:50:45 +0800

    Counting the number of characters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,15 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+  count = null;
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title  = 'The Will Will Web';
     }
+  }
+  getKey(evt: HTMLInputElement) {
+    this.count = evt.value.length;
   }
 }
